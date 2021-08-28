4f8e40683361e728f93813fd01c76de35f678ed4
sonangrai <shonahangrae@gmail.com>
2021-08-28 11:13:50 +0545

    mongoDB connected

--- index.js
+++ index.js
@@ -1,12 +1,23 @@
 import express from "express";
 import dotenv from "dotenv/config";
+import connectDB from "./utlis/connectDB.js";
 
+/**
+ * Creating app instance of express
+ */
 const app = express();
+
+/**
+ * Connecting to Mongo DB
+ */
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("k xa from NODE");
 });
 
-//Running the server
+/**
+ * Running the server
+ */
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`App runing on ${PORT}`));
