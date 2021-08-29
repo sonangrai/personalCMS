b3a543a60ca8e45ef66b74eda2cc783a2b794de7
sonangrai <shonahangrae@gmail.com>
2021-08-29 07:40:46 +0545

    renamed folder and also added user modal

---
+++ models/User.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export default mongoose.model("User", userSchema);
