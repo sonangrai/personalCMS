5d36d46a7b525db99229781bdf199dffc061b806
sonangrai <shonahangrae@gmail.com>
2021-09-19 18:08:21 +0545

    added a servers jsdoc

--- index.js
+++ index.js
@@ -14,8 +14,27 @@ const swaggerDefinition = {
   openapi: "3.0.0",
   info: {
     title: "Express API for JSONPlaceholder",
-    version: "1.0.0",
+    description:
+      "This is a REST API application made with Express. It retrieves data from JSONPlaceholder.",
+    license: {
+      name: "Licensed Under MIT",
+      url: "https://spdx.org/licenses/MIT.html",
+    },
+    contact: {
+      name: "JSONPlaceholder",
+      url: "https://jsonplaceholder.typicode.com",
+    },
   },
+  servers: [
+    {
+      url: "http://localhost:5000",
+      description: "Development server",
+    },
+    {
+      url: "https://zd91a74bb-z43c71d08-gtw.qovery.io/",
+      description: "Production server",
+    },
+  ],
 };
 
 const options = {
