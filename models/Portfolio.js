1081180ffb532b6d00369b0baa2d573920b52505
sonangrai <shonahangrae@gmail.com>
2021-09-28 15:09:06 +0545

    added porfolio modal

---
+++ models/Portfolio.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const PortfolioModal = new mongoose.Schema(
+  {
+    uid: { type: mongoose.Schema.Types.ObjectId, ref: "user" },
+    title: {
+      type: String,
+      required: true,
+    },
+    category: {
+      type: String,
+      required: true,
+    },
+    link: {
+      type: String,
+    },
+    preview: {
+      type: String,
+    },
+    description: {
+      type: String,
+    },
+    gallery: [
+      {
+        imgUrl: { type: String },
+        caption: { type: String },
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Portfolio", PortfolioModal);
