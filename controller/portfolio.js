d149bd02044fcc628f8a1b352729da5741d20aee
sonangrai <shonahangrae@gmail.com>
2021-10-02 07:41:20 +0545

    added auth middleware

--- controller/portfolio.js
+++ controller/portfolio.js
@@ -1,3 +1,19 @@
+const { validationResult } = require("express-validator");
+/**
+ *
+ * @param {Post new porfolio} req
+ * @param {*} res
+ */
 exports.addPortfolio = async (req, res) => {
-  res.send("heello from porfilio");
+  console.log(req.user.id);
+  //Checking params
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+
+  const { title, category, link, preview, description } = req.body;
+
+  try {
+  } catch (error) {}
 };
--- routes/portfolio.js
+++ routes/portfolio.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { check } = require("express-validator");
 const { addPortfolio } = require("../controller/portfolio");
 
 const router = express.Router();
@@ -6,6 +7,13 @@ const router = express.Router();
 /**
  * Post new profolio
  */
-router.post("/portfolio", addPortfolio);
+router.post(
+  "/portfolio",
+  [
+    check("title", "Title is Required Field").notEmpty(),
+    check("category", "Category is Needed").notEmpty(),
+  ],
+  addPortfolio
+);
 
 module.exports = router;
