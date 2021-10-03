60e56a806d27cf4c02e9f00a80e88bb5ffbca582
sonangrai <shonahangrae@gmail.com>
2021-10-03 09:04:55 +0545

    added the contact modal

---
+++ models/Contact.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+
+const ContactModal = new mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    msgType: {
+      type: String,
+      required: true,
+    },
+    message: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Contact", ContactModal);
