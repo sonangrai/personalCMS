542eabae0836821b12d6f8ff5032843f8574875a
sonangrai <shonahangrae@gmail.com>
2021-11-10 20:05:36 +0545

    corssOrigin

--- PersonalCMSfront/pages/_app.js
+++ PersonalCMSfront/pages/_app.js
@@ -11,11 +11,7 @@ const MyApp = ({ Component, pageProps }) => {
     <>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link
-          rel="preconnect"
-          href="https://fonts.gstatic.com"
-          crossOrigin="true"
-        />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
           href="https://fonts.googleapis.com/css2?family=Jost:wght@100;200;400;500;600;700&display=swap"
           rel="stylesheet"
